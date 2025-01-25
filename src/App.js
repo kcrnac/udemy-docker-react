172af39209611afd22ff9c5d82c58f46e1f5f4e8
kcrnac <kcrnacch@gmail.com>
2025-01-25 20:01:02 +0100

    Add GitHub Actions workflow for frontend deployment and fix typo in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React22
+          Learn React
         </a>
       </header>
     </div>
